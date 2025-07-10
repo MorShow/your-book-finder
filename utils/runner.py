b0880390fa58f6204d49b5520a3d68d1151df8cd
aleks <aleksandrkasatmail@gmail.com>
2025-07-10 17:45:41 +0200

    Create a MLflow runner for clustering models saving

--- utils/runner.py
+++ utils/runner.py
@@ -1,38 +1,82 @@
 from model import TopicVectorizerClusterizer
+
 from functools import partial
+from typing import Tuple
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
 from sklearn.metrics import silhouette_score
 
+import mlflow
+import mlflow.sklearn
 import optuna
 # from airflow import DAG
 # from airflow.decorators import task, task_group
 # from airflow.dates import days_ago
 
 
-def model_performance(data: pd.DataFrame, cluser_size: int, cluster_eps: float, cluster_kneigh: int) -> float:
+def model_performance(data: pd.DataFrame, cluser_size: int, cluster_eps: float, cluster_kneigh: int) -> Tuple[object,
+                                                                                                              float]:
     tvc = TopicVectorizerClusterizer(cluser_size, cluster_eps, cluster_kneigh)
     result = tvc.fit_transform(data)
     vectors = np.stack(result['vector'].values)
     clusters = result['cluster'].values
 
     if len(set(clusters)) < 2:
-        return -1
-    return silhouette_score(vectors, clusters)
+        return tvc, -1
+    return tvc, silhouette_score(vectors, clusters)
 
 
 def objective(trial: optuna.trial.Trial, data: pd.DataFrame) -> float:
     row_count, _ = data.shape
     cluster_size = int(trial.suggest_discrete_uniform('min_cluster_size', 2, row_count, 3))
     cluster_epsilon = trial.suggest_loguniform('cluster_epsilon', 1e-5, 100)
-    cluster_kneighbor = trial.suggest_int('cluster_kneighbor', 1, row_count ** 1/2)
-    return model_performance(data, cluster_size, cluster_epsilon, cluster_kneighbor)
+    cluster_kneighbors = trial.suggest_int('cluster_kneighbors', 1, row_count ** 1/2)
+
+    model, score = model_performance(data, cluster_size, cluster_epsilon, cluster_kneighbors)
+
+    if score > objective.best_score:
+        objective.best_score = score
+        objective.best_state = {
+            'min_cluster_size': cluster_size,
+            'cluster_epsilon': cluster_epsilon,
+            'cluster_kneighbors': cluster_kneighbors,
+            'best_score': score,
+            'best_model': model,
+            'best_data': data
+        }
+
+    return score
+
+
+objective.best_score = -float('inf')
+objective.best_state = {}
+
+
+def run(filename: str, n_trials: int):
+    dataset = pd.read_csv(filename)
+    objective_wrapped = partial(objective,
+                                data=dataset)
+    study = optuna.create_study(direction='maximize')
+    study.optimize(objective_wrapped, n_trials=n_trials)
+
+    tag = filename[filename.find('\\') + 1:filename.rfind('.csv')]
+    with mlflow.start_run(run_name="best_model_run"):
+        mlflow.set_tags({
+            'dataset_name': tag,
+            'timestamp': datetime.now().isoformat(),
+            'phase': 'training'
+        })
+        mlflow.log_param('min_cluster_size', objective.best_state['min_cluster_size'])
+        mlflow.log_param('cluster_epsilon', objective.best_state['cluster_epsilon'])
+        mlflow.log_param('cluster_kneighbors', objective.best_state['cluster_kneighbors'])
+        mlflow.log_metric("silhouette_score", objective.best_score)
+        mlflow.sklearn.log_model(objective.best_state['best_model'],
+                                 name="best_model",
+                                 input_example=objective.best_state['best_data'])
 
 
 if __name__ == '__main__':
-    dataset = pd.read_csv(r'C:\Users\aleks\OneDrive\Desktop\Studying\your-book-finder\data\raw'
-                          r'\gutenberq_books_tiny.csv')
-    objective_wrapped = partial(objective, data=dataset)
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective_wrapped, n_trials=100)
+    run(r'C:\Users\aleks\OneDrive\Desktop\Studying\your-book-finder\data\raw\gutenberq_books_tiny.csv',
+        n_trials=20)
